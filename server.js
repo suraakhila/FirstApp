2b719114753c638327caf4bf73b7596c9b136dca
Sura Akhila <126574465+suraakhila@users.noreply.github.com>
2024-10-18 22:23:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const { createServer } = require('node:http');
+const http = require('http');
+const fs = require('fs');
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const server = http.createServer((req, res) => {
+  fs.readFile('index.html', (err, data) => {
+    if (err) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end('Error loading index.html');
+      return;
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(data);
+  });
 });
 
 server.listen(port, hostname, () => {
